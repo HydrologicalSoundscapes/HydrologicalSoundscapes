5af235d4cb62239968a1370cd8b311e5c9775b18
IvanHeriver <ihach_ihach@hotmail.com>
2022-01-24 13:39:19 +0100

    no more prefetch of images (for now)

--- src/App.svelte
+++ src/App.svelte
@@ -18,26 +18,25 @@
         document.addEventListener("click", initToneJS)
     })
 
-    const images_to_preload = [
-        'arrow_down_thick.svg',     
-        'arrow_up_thick.svg',       
-        'map_marker_pin.png',       
-        'map_marker_pin_blue.png',  
-        'map_marker_pin_orange.png',
-        'map_marker_pin_red.png',   
-        'map_marker_shadow.png',    
-        'options.svg',
-        'pause.svg',
-        'play.svg',
-        'stop.svg'
-    ]
+    // const images_to_preload = [
+    //     'arrow_down_thick.svg',     
+    //     'arrow_up_thick.svg',       
+    //     'map_marker_pin.png',       
+    //     'map_marker_pin_blue.png',  
+    //     'map_marker_pin_orange.png',
+    //     'map_marker_pin_red.png',   
+    //     'map_marker_shadow.png',    
+    //     'options.svg',
+    //     'pause.svg',
+    //     'play.svg',
+    //     'stop.svg'
+    // ]
 </script>
-<svelte:head>
+<!-- <svelte:head>
     {#each images_to_preload as image}
-      <!-- <link rel="preload" as="image" href={`images/${image}`} /> -->
       <link rel="prefetch" as="image" href={`images/${image}`} />
     {/each}
-</svelte:head>
+</svelte:head> -->
 <main>
     <Panel />
     <Map />
@@ -45,7 +44,6 @@
 
 <style>
     main {
-        background-color: steelblue;
         position: relative;
     }
 </style>
