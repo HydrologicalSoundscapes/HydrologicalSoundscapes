057263558415f5c0d51f8b94d122f78594a228df
IvanHeriver <ihach_ihach@hotmail.com>
2022-01-25 15:18:33 +0100

    mapping of size variable to barPlot and BPM
    Note that size mapping to BPM is done here (with the rescale function relocated here from sounds.js file). This violate some principles of the code organisation followed so far: sounds.js should handle mapping to sounds related element...

--- src/scripts/dataProcessing.js
+++ src/scripts/dataProcessing.js
@@ -1,3 +1,4 @@
+import {scaleLog} from "d3"
 import { BarChart } from "./plots";
 import { computeMeanMonthlyPart, computeMaxMonthlyPart, computeMinMonthlyPart, ARRANGEMENTS } from "./sounds";
 
@@ -13,7 +14,7 @@ export function meanMonthlyPS(station, old_PS=null, arrangement_id) {
         const chart = BarChart(data_to_plot, {
             x: (d, i) => d[0],
             y: (d, i) => d[1],
-            color: "lightblue", height: 200, yDomain: [0, 0.5],
+            color: "lightblue", height: 200, yDomain: [0, 0.5], yFormat: ".0%",  marginLeft: 30, 
         })
         const part = computeMeanMonthlyPart(data_medium, data_volume, chart.highlight, sound_scale_id)
         return {
@@ -36,7 +37,7 @@ export function maxMonthlyPS(station, old_PS, arrangement_id) {
         const chart = BarChart(data_to_plot, {
             x: (d, i) => d[0],
             y: (d, i) => d[1],
-            color: "lightblue", height: 200,yDomain: [0, 1],
+            color: "lightblue", height: 200,yDomain: [0, 1],yFormat: ".0%",  marginLeft: 30, 
         })
         const part = computeMaxMonthlyPart(data_max, chart.highlight, sound_scale_id)
         return {
@@ -59,7 +60,7 @@ export function minMonthlyPS(station, old_PS, arrangement_id) {
         const chart = BarChart(data_to_plot, {
             x: (d, i) => d[0],
             y: (d, i) => d[1],
-            color: "lightblue", height: 200,yDomain: [0, 1],
+            color: "lightblue", height: 200,yDomain: [0, 1],yFormat: ".0%", marginLeft: 30, 
         })
         const part = computeMinMonthlyPart(data_min, chart.highlight, sound_scale_id)
         return {
@@ -72,4 +73,37 @@ export function minMonthlyPS(station, old_PS, arrangement_id) {
         old_PS.part = computeMinMonthlyPart(data_min, old_PS.plot.highlight, sound_scale_id)
         return old_PS
     }
+}
+
+export function sizePS(station, old_PS) {
+    const size = station.data.size
+    const size_log = {val: Math.log(size.val), min: Math.log(size.min), max: Math.log(size.max)}
+    const rescale_target = [600, 120]
+    const data_to_plot = [["", size.val]]
+    if (!old_PS) {
+        const chart = BarChart(data_to_plot, {
+            x: (d, i) => d[0],
+            y: (d, i) => d[1],
+            yType: scaleLog ,color: "lightblue", height: 700, width: 40, yDomain: [size.min, size.max],
+            marginLeft: 30, 
+        })
+        const bpm = rescale([size_log.val], [size_log.min, size_log.max], rescale_target)[0]
+        return {
+            plot: chart,
+            bpm: bpm
+        }
+    } else {
+        old_PS.plot.update(data_to_plot, {yDomain: [size.min, size.max]})
+        old_PS.bpm = rescale([size_log.val], [size_log.min, size_log.max], rescale_target)[0]
+        return old_PS
+    }
+
+}
+
+export function rescale(values, source, target) {
+    function rescaler(value) {
+        let p = (value - source[0])/(source[1]-source[0])
+        return p * (target[1] - target[0]) + target[0]
+    }
+    return values.map(v=>rescaler(v))
 }
