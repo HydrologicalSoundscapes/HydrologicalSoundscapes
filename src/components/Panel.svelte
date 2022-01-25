6266a124242e0bfc032dafc7e94c9d8010f96274
IvanHeriver <ihach_ihach@hotmail.com>
2022-01-25 15:13:05 +0100

    bug fix: need to refere to the currentStationPS store  in a component always visible to enforce change in sounds

--- src/components/Panel.svelte
+++ src/components/Panel.svelte
@@ -1,10 +1,14 @@
 <script>
-    import {uiOptionPanel, uiPlotPanel} from "../scripts/appState"
+    import {uiOptionPanel, uiPlotPanel, currentStationPS} from "../scripts/appState"
     import Plots from "./Plots.svelte"
     import Options from "./Options.svelte"
     import Header from "./Header.svelte"
     import Loading from "./Loading.svelte";
-    
+
+    $: {
+        // this is needed to enforce change in sounds when plots are hidden
+        console.log("$currentStationPS", $currentStationPS)
+    }
 </script>
 
 <div class="wrapper">
