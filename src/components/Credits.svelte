d94e64fa7fede487935173ace11f4c6bc3b18467
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2022-02-04 10:10:37 +0100

    Update Credits.svelte

--- src/components/Credits.svelte
+++ src/components/Credits.svelte
@@ -1,9 +1,10 @@
 <div class="container">
   <div class="financial">
-    This project has received funding from the European Union’s Horizon 2020
+    This project has received funding from
+    <!-- the European Union’s Horizon 2020
     research and innovation programme under the Marie Sklodowska-Curie grant
     agreement No
-    <a href="https://cordis.europa.eu/project/id/835496">835496</a>.
+    <a href="https://cordis.europa.eu/project/id/835496">835496</a>. -->
   </div>
   <div class="materials">
     Piano and bass samples sounds are from <a
@@ -11,13 +12,13 @@
       target="_blank">http://theremin.music.uiowa.edu/</a
     >
   </div>
-  <div class="ihdev">
-    App created by Ivan Horner - <a href="https://ihdev.fr">ihdev.fr</a>
-  </div>
   <div class="src">
     Source code is available on <a
       href="https://github.com/IvanHeriver/HydrologicalSoundscapes">Github</a
     >
+  </div>
+  <div class="ihdev">
+    App created by Ivan Horner - <a href="https://ihdev.fr">ihdev.fr</a>
   </div>
 </div>
 
